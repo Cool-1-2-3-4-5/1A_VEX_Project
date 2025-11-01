529fb241b974719bee2e645116284aa4126141ca
Elil Thirumugam <elilgrade8@gmail.com>
2025-11-01 19:09:43 -0400

    BumpCheck

--- include/drivetrain.hpp
+++ include/drivetrain.hpp
@@ -8,6 +8,7 @@ class Drivetrain
     motor right_;
     inertial BrainInertial;
     brain Brain;
+    bumper Bumper;
 
 public:
     Drivetrain(char left_Port, char right_Port):
@@ -68,7 +69,7 @@ public:
         left_.stop();
         right_.stop();
     }
-    void PIDturn(float kp, float ki, float kd, float angle){
+    void PIDturn(float kp, float ki, float kd, float angle, float current_angle){
         Brain.Screen.clearScreen();
         float average_dist = 0;
         float derivate_difference;
@@ -81,6 +82,7 @@ public:
         timeout.reset();
         left_.spin(forward);
         right_.spin(forward);
+        
         while(check){
             average_dist = (left_.position(degrees)+right_.position(degrees))/2;
             //potential
@@ -90,8 +92,8 @@ public:
             // derivative
             derivate_difference = error - prev_error;
             speed = kp*(error) + ki*cumerror + kd*derivate_difference;
-            left_.setVelocity(speed, percent);
-            right_.setVelocity(speed, percent);
+            left_.setVelocity(-1*speed*changer, percent);
+            right_.setVelocity(speed*changer, percent);
             prev_error = error;
             if(timeout.time(sec) > 3.0) {
                 check = false;
@@ -101,5 +103,29 @@ public:
         }
         left_.stop();
         right_.stop();
+    }
+    bool bumpMove(float dist_to_middle){
+        left_.setPosition(0,degrees);
+        right_.setPosition(0,degrees);
+        const float tolerance = 0.5;
+        float current_distance;
+        float difference;
+        left_.spin(forward);
+        right_.spin(forward);
+        while(!Bumper.pressing() || (((left_.position(degrees) + right_.position(degrees))/2.0) *(200.0/360)) < dist_to_middle){
+        }
+        left_.stop(brakeType::hold);
+        right_.stop(brakeType::hold);
+        current_distance = ((left_.position(degrees) + right_.position(degrees))/2.0) * (200.0/360);
+        difference = current_distance - dist_to_middle;
+        if(difference < 0){
+            difference = -difference;
+        }
+        if(difference <= tolerance){
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,39 @@
 #include "vex.h"
-
+#include <cmath>
 using namespace vex;
 vex::brain Brain;
 Drivetrain drive(PORT1,PORT6);
 
 int main() {
     // wait(1,seconds);
+    float angle = 0;
+    int x_grid = 5;
+    int y_grid = 5; 
+    int x_pos = 1;
+    int y_pos = 1;
+    int plant = int[x_grid][y_grid];
+    if (drive.bumpMove()){
+        if(angle == 0){
+            y_pos++;
+            //append x_pos,y_pos to array
+            y_pos--;
+        }
+        else if(angle == 90){
+            x_pos++;
+            //append x_pos,y_pos to array
+            x_pos--;
+        }
+        else if(angle == 180){
+            y_pos--;
+            //append x_pos,y_pos to array
+            y_pos++;
+        }
+        else{
+            x_pos--;
+            //append x_pos,y_pos to array
+            x_pos++;
+        }
+    }
     Brain.programStop();
     while(1) {
         this_thread::sleep_for(10);
